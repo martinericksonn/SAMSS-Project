d3fde964e357d6f1da8c93679d349a3ed55fadec
martin <lapetajemartin@gmail.com>
2023-03-26 18:39:42 +0800

    add users route

--- _backend-express/routes/student.js
+++ _backend-express/routes/student.js
@@ -2,80 +2,70 @@ const express = require("express");
 const router = express.Router();
 const { CRUDReturn } = require("../modules/crud-return-interface");
 
-
-
-
 router.post("/add", async (req, res, next) => {
-    try {
-        console.log(req.body);
-        const { test } = req.body;
-        // add code to add a new student
-        
-        
-        
-        res.status(200).send(new CRUDReturn(true, {}, "post").json());
-    } catch (error) {
-        next(error)    
-    }
-    
+  try {
+    console.log(req.body);
+    const { test } = req.body;
+    // add code to add a new student
+
+    res.status(200).send(new CRUDReturn(true, {}, "post").json());
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.post("/students/subjects", async (req, res) => {
-    try {
-        const { studentNo, subject } = req.body;
-        // add code to add a new subject for a student
-        res
-          .status(200)
-          .send(new CRUDReturn(true, { studentNo, subject }, "post").json());
-    } catch (error) {
-        next(error)
-    }
+router.post("/add-subject", async (req, res) => {
+  try {
+    const { studentNo, subject } = req.body;
+    // add code to add a new subject for a student
+    res
+      .status(200)
+      .send(new CRUDReturn(true, { studentNo, subject }, "post").json());
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.delete("/students/subjects", async (req, res) => {
-    try {
-        
-        const { studentNo, subject } = req.body;
-        // add code to remove a subject from a student
-        res
-          .status(200)
-          .send(new CRUDReturn(true, { studentNo, subject }, "delete").json());
-    } catch (error) {
-        next(error)
-    }
+router.delete("/remove-subject", async (req, res) => {
+  try {
+    const { studentNo, subject } = req.body;
+    // add code to remove a subject from a student
+    res
+      .status(200)
+      .send(new CRUDReturn(true, { studentNo, subject }, "delete").json());
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.delete("/students", async (req, res) => {
-    try {
-        
-        const id = req.params.id;
-      
-        res.status(200).send(new CRUDReturn(true, { id }, "delete").json());
-    } catch (error) {
-        next(error)
-    }
+router.delete("/", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    res.status(200).send(new CRUDReturn(true, { id }, "delete").json());
+  } catch (error) {
+    next(error);
+  }
   // add code to delete a student
 });
 
-router.get("/students", async (req, res) => {
-    try {
-        
-        const id = req.params.id;
-        // add code to get a student by id
-        res.status(200).send(new CRUDReturn(true, { id }, "get").json());
-    } catch (error) {
-        next(error)
-    }
+router.get("/", async (req, res) => {
+  try {
+    const id = req.params.id;
+    // add code to get a student by id
+    res.status(200).send(new CRUDReturn(true, { id }, "get").json());
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get("/students", async (req, res) => {
-    try {
-        
-        // add code to get all students
-        res.status(200).send(new CRUDReturn(true, {}, "get").json());
-    } catch (error) {
-        next(error)
-    }
+router.get("/all", async (req, res) => {
+  try {
+    // add code to get all students
+    res.status(200).send(new CRUDReturn(true, {}, "get").json());
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get("/", (req, res) => {
--- _backend-express/server.js
+++ _backend-express/server.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const ErrorHandler = require("./middlewares/error-handler")
+const ErrorHandler = require("./middlewares/error-handler");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 // Start server
@@ -14,14 +14,16 @@ app.get('/', (req, res) => {
 // }));
 app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
-const  attendanceRouter = require("./routes/attendance");
-const studentRouter = require("./routes/student");
+const attendance = require("./routes/attendance");
+const student = require("./routes/student");
+const user = require("./routes/user");
 
 app.use(cors());
 
-app.use("/attendance",attendanceRouter)
-app.use("/student",studentRouter)
-app.use(ErrorHandler)
+app.use("/attendance", attendance);
+app.use("/student", student);
+app.use("/user", user);
+app.use(ErrorHandler);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
