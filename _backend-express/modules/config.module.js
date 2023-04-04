7d16d3926c795f022150d7c6c5614587a72000ee
martin <lapetajemartin@gmail.com>
2023-04-05 00:14:02 +0800

    Add config module db query

--- _backend-express/modules/config.module.js
+++ _backend-express/modules/config.module.js
@@ -18,6 +18,29 @@ class ConfigModule {
 
     return resultMap;
   }
+
+  async editAll(config) {
+    const { current_term, current_year, late_minutes, absent_minutes } = config;
+    const [result] = await pool.query(
+      "UPDATE config SET current_term = ?, current_year = ?, late_minutes = ?, absent_minutes = ? WHERE id = 1",
+      [current_term, current_year, late_minutes, absent_minutes]
+    );
+    return result.affectedRows === 1;
+  }
+
+  async getAll() {
+    const [rows] = await pool.query("SELECT * FROM config");
+    return rows;
+  }
+
+  async add(config) {
+    const { current_term, current_year, late_minutes, absent_minutes } = config;
+    const [result] = await pool.query(
+      "INSERT INTO config (current_term, current_year, late_minutes, absent_minutes) VALUES (?, ?, ?, ?)",
+      [current_term, current_year, late_minutes, absent_minutes]
+    );
+    return result.insertId;
+  }
 }
 
 module.exports = { ConfigModule };
